61ae3d76fce8755be12c802a805926d8ba528b2b
Evan Bouman <evan.bouman@gmail.com>
2024-08-15 10:54:28 -0500

    Created partsNeeded. Created supplyChanges. Created secondItem. Removed last value in supplyChanges.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,22 +5,26 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
-
+let partsNeeded = 40
+console.log(`We need this many parts: `, partsNeeded);
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
-
+let supplyChanges = [3, 5, -6, 0, 7, 11]
+console.log(`Supply changes: `, supplyChanges);
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
+let secondItem = supplyChanges[1];
+console.log(`The second item is`, secondItem);
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
-
+let removedItem = supplyChanges.pop();
+console.log(`Removed the last item from supplyChanges:`, removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
